7b7ba6fb46c09e7447f18109a36d57db0afbbb02
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-11 16:17:06 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,12 +1,22 @@
 import groovy.yaml.YamlSlurper
 
-def call (){
-  
-    def exampleYaml = '''\
-    ---
-    - subject: "maths"
-    - subject: "chemistry"
-    '''
-    
-  println exampleYaml
-}
+def configYaml = '''\
+---
+application: "Sample App"
+users:
+- name: "mrhaki"
+  likes:
+  - Groovy
+  - Clojure
+  - Java
+- name: "Hubert"
+  likes:
+  - Apples
+  - Bananas
+connections:
+- "WS1"
+- "WS2"
+'''
+
+// Parse the YAML.
+def config = new YamlSlurper().parseText(configYaml)
