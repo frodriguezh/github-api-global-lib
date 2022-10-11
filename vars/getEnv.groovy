07a4838155ce8797a7c5229487a327e18c1ed69f
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-11 17:52:32 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -5,5 +5,7 @@ def call(Map config = [:]){
  println data.environment."${config.branchTarget}".urlserver
  println data.environment."${config.branchTarget}".idsecret
  
+ println "${URL_SERVER}"
+ 
 }
 
