0de7cfff71bab1124b2faaf9a6890cc529bb4d5f
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-11 17:40:32 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -2,8 +2,8 @@ def call(String branchTarget){
  
  def data = readYaml file: "resources/com/planetpope/envconfig.yaml"
 
- println data.environment.${branchTarget}.urlserver
- println data.environment.${branchTarget}.idsecret
+ println "data.environment.${branchTarget}.urlserver"
+ println "data.environment.${branchTarget}.idsecret"
  
 }
 
