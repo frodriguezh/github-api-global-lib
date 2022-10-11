2e675dabf8b0119ad959c3100deed704b07393e9
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-11 17:17:08 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -6,9 +6,9 @@ something: 'my datas'
 size: 3
 isEmpty: false
 """
- assert datas.something == 'my datas'
- assert datas.size == 3
- assert datas.isEmpty == false
+ println datas.something
+ println datas.size
+ println datas.isEmpty
  
 }
 
