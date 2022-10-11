5f7204623a1baf8beb2c717c4280fe076c310598
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-11 15:49:41 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,5 +1,13 @@
 import groovy.yaml.YamlSlurper
-
-List example = new YamlSlurper().parse("envconfig.yaml" as File)
     
-example.each{println it.environment}
+    def exampleYaml = '''\
+    ---
+    - subject: "maths"
+    - subject: "chemistry"
+    '''
+    
+    List example = new YamlSlurper().parseText(exampleYaml)
+//If your source is a File
+//    List example = new YamlSlurper().parse("example.yaml" as File)
+    
+    example.each{println it.subject}
