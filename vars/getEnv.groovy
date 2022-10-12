202c7f2f64d83715a22bc1b46b42dc69171c4b46
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-12 17:30:04 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,18 +1,11 @@
 def call(Map config = [:]){
  
+ sh "exit 1"
+ 
  def data = readYaml file: "resources/com/planetpope/envconfig.yaml"
 
- //println data.environment."${config.branchTarget}".urlserver
- //println data.environment."${config.branchTarget}".idsecret
- 
  env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
  env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
  
- def s = 'Argh, Groovy you say, mate?'
- 
- String encoded = s.bytes.encodeBase64().toString()
- 
- println "${encoded}"
-
 }
 
