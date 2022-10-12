ce5eadfa072ca3a137d8086526e2788caa199fda
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-12 17:34:55 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -6,6 +6,6 @@ def call(Map config = [:]){
  env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
  env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
  
- assert env.URL_SERVER == "wngplgmaappwd00.itauchile.cl" : "Build fails 0"
+ assert env.URL_SERVER != null : "errorrrrrrr"
  
 }
