76fa92f572befc524efd7d1ab1b13c872507e0fc
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-12 17:33:48 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,11 +1,11 @@
 def call(Map config = [:]){
  
- sh "exit 1"
  
  def data = readYaml file: "resources/com/planetpope/envconfig.yaml"
 
  env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
  env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
  
+ assert env.URL_SERVER == "wngplgmaappwd00.itauchile.cl" : "Build fails 0"
+ 
 }
-
