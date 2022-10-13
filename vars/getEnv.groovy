cc35e1511a9f428c9e243fec67e8d1142b0be6a7
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-13 18:12:29 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -10,7 +10,7 @@ if (exists) {
  
  if (data?.environment) {
   
-  assert if(data.environment."${config.branchTarget}".urlserver) : "nulo"
+  assert (data.environment."${config.branchTarget}".urlserver) : "nulo"
   
   env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
   env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
