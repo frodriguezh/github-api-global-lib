a2f3451bf9fd5c1edb731ffc7e3b7ae4889c1822
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-13 16:03:47 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,23 +1,30 @@
 def call(Map config = [:]){
  
- def envconfig = "resources/com/planetpope/envconfig.yaml"
- 
- def exists = fileExists envconfig
- if (exists) {
-       echo 'existe archivo'
-       sh 'exit 0;'
-   } else {
-       echo 'no existe archivo'
-       sh 'exit 1;'
-   }
+def envconfig = "resources/com/planetpope/envconfig.yaml"
 
- def data = readYaml file: envconfig
+def exists = fileExists envconfig
 
- env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
- env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
+if (exists) {
  
- println env.URL_SERVER
- println env.ID_SECRET
+  //echo 'existe archivo'
+  //sh 'exit 0;'
+  def data = readYaml file: envconfig
+  println data.size
+
+  env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
+  env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
+
+  println env.URL_SERVER
+  println env.ID_SECRET
+ 
+} else {
+
+  echo "No existe archivo de configuracion ${envconfig}"
+  sh 'exit 1;'
+ 
+}
+
+
  //assert env.URL_SERVER != "wngplgmaappwd00.itauchile.cl" : "**********************El DNS es igual a wngplgmaappwd00.itauchile.cl**********************"
  
 }
