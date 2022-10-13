dc5f419fdf5f61b4aaf554729690e0c2d60a0757
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-13 15:37:02 -0300

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,6 +1,14 @@
 def call(Map config = [:]) {
-    loadLinuxScript(name: 'hello-world.sh')
+    //loadLinuxScript(name: 'hello-world.sh')
     //sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
-    sh "./hello-world.sh ${config.name}"
-   
+    //sh "./hello-world.sh ${config.name}"
+   //def dir = readFile("${WORKSPACE}/Jenkinsfile");
+   //println fileExists dir
+   def exists = fileExists "${WORKSPACE}/Jenkinsfile"
+
+    if (exists) {
+        echo 'Yes'
+    } else {
+        echo 'No'
+    }
 }
