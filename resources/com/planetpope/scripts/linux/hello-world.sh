a2c0be3252ecff6a9acd5552334a35fb435b95b7
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-13 12:40:51 -0300

    Update hello-world.sh

--- resources/com/planetpope/scripts/linux/hello-world.sh
+++ resources/com/planetpope/scripts/linux/hello-world.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 //echo Hello $1. Today is $2.
-if ! cat $1; then
-    echo "No existe archivo: $1"
-    exit 1;
-fi
+//if ! cat $1; then
+    //echo "No existe archivo: $1"
+    //exit 1;
+//fi
 
 if test -f "$1"; then
     echo "$1 exists."
