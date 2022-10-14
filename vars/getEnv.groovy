b17b2acf8561454c4c5245e27dee9a697b39d766
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-14 11:17:47 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -4,35 +4,22 @@ def envconfig = "resources/com/planetpope/envconfig.yaml"
 
 def exists = fileExists envconfig
 
-if (exists) {
- 
- def data = readYaml file: envconfig
- 
- if (data?.environment) {
-  
-  assert (data?.environment."${config.branchTarget}".urlserver): "\nError: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.urlserver"
-  assert (data?.environment."${config.branchTarget}".idsecret): "\nError: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.idsecret"
-  
-  env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
-  env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
-  
-  println env.URL_SERVER
-  println env.ID_SECRET
+  if (exists) {
 
- }
- else{
-  
-  echo "\nError: El archivo de configuracion ${envconfig} no tiene ambientes declarados"
-  sh 'exit 1;'
-  
- }
- 
-} else {
+   def data = readYaml file: envconfig
 
-  echo "\nError: No existe archivo de configuracion ${envconfig}"
-  sh 'exit 1;'
- 
-}
+   assert (data?.environment."${config.branchTarget}".urlserver): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.urlserver"
+   assert (data?.environment."${config.branchTarget}".idsecret): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.idsecret"
+
+   env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
+   env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
+
+  } else {
+
+    echo "\nErrores: No existe archivo de configuracion ${envconfig}"
+    sh 'exit 1;'
+
+  }
 
  
 }
