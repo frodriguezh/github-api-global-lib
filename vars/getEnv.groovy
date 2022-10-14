3cb9e6cfd1d4d9b6b5fde26b44ee060cf3c6e594
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-14 11:49:16 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,29 +1,21 @@
 def call(Map config = [:]){
  
-  def envconfig = "resources/como/planetpope/envconfig.yaml"
+  def envconfig = "resources/com/planetpope/envconfig.yaml"
   String[] configvalues = ["serverurl","secretid","appsettings","tmpfolder"]; 
 
   def exists = fileExists envconfig
- 
+
   assert (exists): "\nErrores: No existe archivo de configuracion ${envconfig}"
+
+  def data = readYaml file: envconfig
+
+  for(int i in configvalues) { 
+   assert (data?.environment."${config.branchTarget}"."${i}"): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.${i}" 
+  } 
+
+  env.SERVER_URL = data.environment."${config.branchTarget}".serverurl
+  env.SECRET_ID = data.environment."${config.branchTarget}".secretid
+  env.APP_SETTINGS = data.environment."${config.branchTarget}".appsettings
+  env.TMP_FOLDER = data.environment."${config.branchTarget}".tmpfolder
  
-  if (exists) {
-
-   def data = readYaml file: envconfig
-   
-   for(int i in configvalues) { 
-    assert (data?.environment."${config.branchTarget}"."${i}"): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.${i}" 
-   } 
-
-   env.SERVER_URL = data.environment."${config.branchTarget}".serverurl
-   env.SECRET_ID = data.environment."${config.branchTarget}".secretid
-   env.APP_SETTINGS = data.environment."${config.branchTarget}".appsettings
-   env.TMP_FOLDER = data.environment."${config.branchTarget}".tmpfolder
- 
-  } else {
-
-    echo "\nErrores: No existe archivo de configuracion ${envconfig}"
-    sh 'exit 1;'
-
-  }
 }
