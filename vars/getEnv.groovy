17b39f2d5bb463924d06f3876221cc9caff97a6f
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-14 11:45:02 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,28 +1,29 @@
 def call(Map config = [:]){
  
-def envconfig = "resources/com/planetpope/envconfig.yaml"
-String[] array = ["serverurl","secretid","appsettings","tmpfolder"]; 
+  def envconfig = "resources/com/planetpope/envconfig.yaml"
+  String[] configvalues = ["serverurl","secretid","appsettings","tmpfolder"]; 
 
-def exists = fileExists envconfig
-
+  def exists = fileExists envconfig
+ 
+  assert (exists): echo "\nErrores: No existe archivo de configuracion ${envconfig}"
+ 
   if (exists) {
 
    def data = readYaml file: envconfig
    
-   for(int i in array) { assert (data?.environment."${config.branchTarget}"."${i}"): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.${i}" } 
+   for(int i in configvalues) { 
+    assert (data?.environment."${config.branchTarget}"."${i}"): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.${i}" 
+   } 
 
    env.SERVER_URL = data.environment."${config.branchTarget}".serverurl
    env.SECRET_ID = data.environment."${config.branchTarget}".secretid
    env.APP_SETTINGS = data.environment."${config.branchTarget}".appsettings
    env.TMP_FOLDER = data.environment."${config.branchTarget}".tmpfolder
  
-
   } else {
 
     echo "\nErrores: No existe archivo de configuracion ${envconfig}"
     sh 'exit 1;'
 
   }
-
- 
 }
