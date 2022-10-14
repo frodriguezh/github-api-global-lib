60a8b1944bc2fb8b0000fbf608c54a9de556b1f8
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-14 11:39:19 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]){
  
 def envconfig = "resources/com/planetpope/envconfig.yaml"
+String[] array = ["serverurl","secretid","appsettings","tmpfolder"]; 
 
 def exists = fileExists envconfig
 
@@ -8,22 +9,13 @@ def exists = fileExists envconfig
 
    def data = readYaml file: envconfig
    
-   String[] array = ["serverurl","secretid","appsettings","tmpfolder"]; 
-		   for(int i in array) { 
-      println(i); 
-   } 
+   for(int i in array) { assert (data?.environment."${config.branchTarget}"."${i}"): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.${i}" } 
 
-   assert (data?.environment."${config.branchTarget}".serverurl): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.serverurl"
-   assert (data?.environment."${config.branchTarget}".secretid): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.secretid"
-   assert (data?.environment."${config.branchTarget}".appsettings): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.appsettings"
-   assert (data?.environment."${config.branchTarget}".tmpfolder): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.idsecret"
-   
    env.SERVER_URL = data.environment."${config.branchTarget}".serverurl
    env.SECRET_ID = data.environment."${config.branchTarget}".secretid
    env.APP_SETTINGS = data.environment."${config.branchTarget}".appsettings
    env.TMP_FOLDER = data.environment."${config.branchTarget}".tmpfolder
  
-   println env.SERVER_URL
 
   } else {
 
