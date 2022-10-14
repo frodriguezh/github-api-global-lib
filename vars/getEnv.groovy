14c5902bbb3b217352f38c098d628ba49f6744f9
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-14 11:23:25 -0300

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -10,9 +10,13 @@ def exists = fileExists envconfig
 
    assert (data?.environment."${config.branchTarget}".urlserver): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.urlserver"
    assert (data?.environment."${config.branchTarget}".idsecret): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.idsecret"
-
+   assert (data?.environment."${config.branchTarget}".appsettings): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.appsettings"
+   assert (data?.environment."${config.branchTarget}".tmpfolder): "\nErrores: El archivo de configuracion ${envconfig} no tiene valor en el tag data.environment.${config.branchTarget}.idsecret"
+   
    env.URL_SERVER = data.environment."${config.branchTarget}".urlserver
    env.ID_SECRET = data.environment."${config.branchTarget}".idsecret
+   env.APP_SETTINGS = data.environment."${config.branchTarget}".appsettings
+   env.TMP_FOLDER = data.environment."${config.branchTarget}".tmpfolder
 
   } else {
 
