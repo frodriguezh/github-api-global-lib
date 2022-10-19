807323be83274be188f35f9dd17f8f4fc607103b
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-19 12:44:54 -0300

    Update existFolder.groovy

--- vars/existFolder.groovy
+++ vars/existFolder.groovy
@@ -2,6 +2,6 @@ def call(Map config = [:]){
  
   def exists = fileExists config.nameFolder
 
-  assert (exists): "\nErrores: No existe archivo de configuracion ${envconfig}"
+  assert (exists): "\nErrores: No existe archivo de configuracion"
   
 }
