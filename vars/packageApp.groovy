573f1c0429bf6fccd4eef9b20d13c5312bea0d12
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-20 13:25:48 -0300

    Update packageApp.groovy

--- vars/packageApp.groovy
+++ vars/packageApp.groovy
@@ -13,6 +13,7 @@ def call(Map config = [:]){
  
   ZipFile zip_file = new ZipFile("${WORKSPACE}/GEMA_NUBE_${BUILD_NUMBER}.zip")
  
-  //assert (file.size()): "\nErrores: El archivo zip GEMA_NUBE_${BUILD_NUMBER}.zip tiene un peso igual a cero"
-  println zip_file.size().toString()
+  assert (zip_file.size() == 0): "\nErrores: El archivo zip GEMA_NUBE_${BUILD_NUMBER}.zip tiene un peso igual a cero"
+ 
+  //println zip_file.size().toString()
 }
