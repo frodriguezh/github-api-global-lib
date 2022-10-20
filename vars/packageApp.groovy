0a0375bbca3cd00e22f7da2d6e3b2b3407529353
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-20 12:44:31 -0300

    Update packageApp.groovy

--- vars/packageApp.groovy
+++ vars/packageApp.groovy
@@ -1,9 +1,13 @@
 def call(Map config = [:]){
  
-   sh '''
-   
-     zip -r GEMA_NUBE_${BUILD_NUMBER}.zip /"'''+config.nameFolder+'''"
-    
-   '''
+  sh '''
+
+    zip -r GEMA_NUBE_${BUILD_NUMBER}.zip /"'''+config.nameFolder+'''"
+
+  '''
+
+  def exists = fileExists "GEMA_NUBE_${BUILD_NUMBER}.zip"
+
+  assert (exists): "\nErrores: No existe el archivo de zip GEMA_NUBE_${BUILD_NUMBER}.zip"
  
 }
