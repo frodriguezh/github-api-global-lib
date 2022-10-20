3fee3319a3a8c97243b8d37d87879290ecc1a47a
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-20 13:29:44 -0300

    Update packageApp.groovy

--- vars/packageApp.groovy
+++ vars/packageApp.groovy
@@ -11,9 +11,9 @@ def call(Map config = [:]){
 
   assert (exists): "\nErrores: No existe el archivo zip GEMA_NUBE_${BUILD_NUMBER}.zip"
  
-  ZipFile zip_file = new ZipFile("${WORKSPACE}/GEMA_NUBE_${BUILD_NUMBER}.zip")
+  ZipFile zipFile = new ZipFile("${WORKSPACE}/GEMA_NUBE_1000.zip")
  
-  assert (zip_file.size() > 0): "\nErrores: El archivo zip GEMA_NUBE_${BUILD_NUMBER}.zip tiene un peso igual a cero"
+  assert (zipFile.size() > 0): "\nErrores: El archivo zip GEMA_NUBE_${BUILD_NUMBER}.zip tiene un peso igual a cero"
  
-  //println zip_file.size().toString()
+
 }
