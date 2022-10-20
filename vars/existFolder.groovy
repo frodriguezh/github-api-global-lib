8f503620a6f4d74938f3f8d4401387035deae8f7
frodriguezh <89743757+frodriguezh@users.noreply.github.com>
2022-10-20 11:54:39 -0300

    Update existFolder.groovy

--- vars/existFolder.groovy
+++ vars/existFolder.groovy
@@ -3,15 +3,15 @@ def call(Map config = [:]){
    sh '''
    
      dotnet restore
-     dotnet publish -c release -o "'''+config.nameFolder+'''" --no-restore
+     dotnet publish -c release -o /"'''+config.nameFolder+'''" --no-restore
      
-     if ! [ -e /"'''+config.nameFolder+'''" ]
+     if ! [ -e "/'''+config.nameFolder+'''" ]
      then 
           echo "\nErrores: No existe carpeta."
           exit 1
      fi
 
-     if ! [ "$(ls -A /"'''+config.nameFolder+'''")" ]
+     if ! [ "$(ls -A "/'''+config.nameFolder+'''")" ]
      then 
           echo "\nErrores: Carpeta vacia."
           exit 1
